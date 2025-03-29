1c427bafe8b26ef3c39fe076d3592dde2b5f5915
margaux-works <margaux.espinasse@gmail.com>
2025-03-29 11:06:51 +0100

    chore: configure Flask-SQLAlchemy

--- app.py
+++ app.py
@@ -1,21 +1,37 @@
 
+import os 
+
 from flask import Flask
 from flask_smorest import Api
+
+from db import db
+
+import models
 
 from resources.property import blp as PropertyBlueprint
 from resources.user import blp as UserBlueprint
 
-app = Flask(__name__)
+def create_app(db_url=None):
+    app = Flask(__name__)
+    app.config["PROPAGATE_EXCEPTIONS"] = True
+    app.config["API_TITLE"] = "Stores REST API"
+    app.config["API_VERSION"] = "v1"
+    app.config["OPENAPI_VERSION"] = "3.0.3"
+    app.config["OPENAPI_URL_PREFIX"] = "/"
+    app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+    app.config[
+        "OPENAPI_SWAGGER_UI_URL"
+    ] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.init_app(app)
 
-app.config["PROPAGATE_EXCEPTIONS"] = True
-app.config["API_TITLE"] = "Immo REST API"
-app.config["API_VERSION"] = "v1"
-app.config["OPENAPI_VERSION"] = "3.0.3"
-app.config["OPENAPI_URL_PREFIX"] = "/"
-app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
-app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    api = Api(app)
 
-api = Api(app)
+    with app.app_context():
+        db.create_all()
 
-api.register_blueprint(PropertyBlueprint)
-api.register_blueprint(UserBlueprint)
+    api.register_blueprint(PropertyBlueprint)
+    api.register_blueprint(UserBlueprint)
+
+    return app
