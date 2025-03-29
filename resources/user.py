390dcbf322046c1219aa36c291ccc21705c59a79
margaux-works <margaux.espinasse@gmail.com>
2025-03-29 13:57:50 +0100

    feat: add user login endpoint with JWT token return

--- resources/user.py
+++ resources/user.py
@@ -1,6 +1,7 @@
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from sqlalchemy.exc import SQLAlchemyError
+from flask_jwt_extended import create_access_token
 from passlib.hash import pbkdf2_sha256
 
 from db import db
@@ -51,6 +52,8 @@ class User(MethodView):
 
         db.session.commit()
         return user
+
+ # User Authentication - Register & Login
     
 @blp.route("/register")
 class UserRegister(MethodView):
@@ -71,3 +74,17 @@ class UserRegister(MethodView):
         db.session.commit()
 
         return {"message": "User created successfully."}, 201
+    
+@blp.route("/login")
+class UserLogin(MethodView):
+    @blp.arguments(UserSchema)
+    def post(self, user_data):
+        user = UserModel.query.filter(
+            UserModel.username == user_data["username"]
+        ).first()
+
+        if user and pbkdf2_sha256.verify(user_data["password"], user.password):
+            access_token = create_access_token(identity=str(user.id))
+            return {"access_token": access_token}, 200
+
+        abort(401, message="Invalid credentials.")
