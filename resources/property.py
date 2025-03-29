1a29ac660b826c6200ab1b7826dae49d2237ab43
margaux-works <margaux.espinasse@gmail.com>
2025-03-29 11:59:07 +0100

    feat: integrate SQLAlchemy and update all endpoints to use models

--- resources/property.py
+++ resources/property.py
@@ -1,10 +1,12 @@
-import uuid
 from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
+from sqlalchemy.exc import SQLAlchemyError, IntegrityError
 
 from schemas import PropertySchema, PropertyUpdateSchema
-from db import properties
+
+from db import db
+from models.property import PropertyModel
 
 
 blp = Blueprint("properties", __name__, description="Operations on properties")
@@ -14,15 +16,23 @@ class PropertyList(MethodView):
     # Endpoint: GET all properties
     @blp.response(200, PropertySchema(many=True))
     def get(self):
-        return list(properties.values())
+        return PropertyModel.query.all()
     
     # Endpoint: POST (create) a new property
     @blp.arguments(PropertySchema)
     @blp.response(201, PropertySchema)
-    def post(self, property_data):        
-        property_id = uuid.uuid4().hex
-        property = {**property_data, "id": property_id}
-        properties[property_id] = property
+    def post(self, property_data):
+        property = PropertyModel(**property_data)
+        try:
+           db.session.add(property)
+           db.session.commit()
+        except IntegrityError:
+           abort(
+               400,
+               message="A property with that name already exists."
+           )
+        except SQLAlchemyError:
+           abort(500, message="An error occurred while creating the property")
         return property
 
 
@@ -31,35 +41,36 @@ class Property(MethodView):
     # Endpoint: GET a specific property
     @blp.response(200, PropertySchema)
     def get(self, property_id):
-        try:
-            return properties[property_id]
-        except KeyError:
-            abort(404, message="Property not found")
+        property = PropertyModel.query.get_or_404(property_id)
+        return property
 
     # Endpoint: PUT (update) a property
     @blp.arguments(PropertyUpdateSchema)
     @blp.response(200, PropertySchema)
-    def put(self, property_data, property_id):        
-        try:
-            property = properties[property_id]
-        except KeyError:
-            abort(404, message="Property not found")
+    def put(self, property_data, property_id):
+        property = PropertyModel.query.get(property_id)
 
-        # Update only provided fields
-        for key in ["name", "description", "property_type", "city", "rooms", "owner_id"]:
-            if key in property_data:
-                property[key] = property_data[key]
+        if not property:
+            abort(404, message="Property not found.")
+
+        for key, value in property_data.items():
+            setattr(property, key, value)
+
+        db.session.commit()
         return property
 
 
 @blp.route("/properties/filter")
 class PropertyFilter(MethodView):
     # Endpoint 3: GET (filter) a property by city
+    @blp.response(200, PropertySchema(many=True))
     def get(self):
         city = request.args.get("city")
         if not city:
             abort(400, message="Missing city in query parameter")
-        filtered = [prop for prop in properties.values() if prop["city"].lower() == city.lower()]
+        filtered = PropertyModel.query.filter(
+            PropertyModel.city.ilike(f"%{city}%")
+        ).all()
         if not filtered:
             abort(404, message=f"No property currently in {city}")
-        return {"Properties": filtered}
+        return filtered
--- resources/user.py
+++ resources/user.py
@@ -1,8 +1,11 @@
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
+from sqlalchemy.exc import SQLAlchemyError
 
 from schemas import UserSchema, UserUpdateSchema
-from db import users
+
+from db import db
+from models.user import UserModel
 
 blp = Blueprint("users", __name__, description="Operations on users")
 
@@ -11,26 +14,40 @@ class UserList(MethodView):
     # Endpoint: GET all users
     @blp.response(200, UserSchema(many=True))
     def get(self):
-        return list(users.values())
+        return UserModel.query.all()
+    
+    # Endpoint: POST (create) a new user
+    @blp.arguments(UserSchema)
+    @blp.response(201, UserSchema)
+    def post(self, user_data):
+        user = UserModel(**user_data)
+        try:
+            db.session.add(user)
+            db.session.commit()
+        except SQLAlchemyError:
+            abort(500, message="An error occurred while creating the user.")
+        return user
 
-@blp.route("/users/<string:id>")
+@blp.route("/users/<string:user_id>")
 class User(MethodView):
     # Endpoint: GET a single user
     @blp.response(200, UserSchema)
-    def get(self, id):
-        try:
-            return users[id]
-        except KeyError:
-            abort(404, message="User not found")
+    def get(self, user_id):
+        user = UserModel.query.get_or_404(user_id)
+        return user
 
     # Endpoint: PUT (update) user data
     @blp.arguments(UserUpdateSchema)
     @blp.response(200, UserSchema)
-    def put(self, user_data, id):
-        user = users.get(id)
+    def put(self, user_data, user_id):
+        user = UserModel.query.get(user_id)
+
         if not user:
-            abort(400, message="User not found")
-        for key in ["first_name", "last_name", "birthdate"]:
-            if key in user_data:
-                user[key] = user_data[key]
+            abort(404, message="User not found.")
+
+        for key, value in user_data.items():
+            setattr(user, key, value)
+
+        db.session.commit()
         return user
+    
