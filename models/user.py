888afa5455124494028b8d031dc65a8b26c9eb11
margaux-works <margaux.espinasse@gmail.com>
2025-03-29 13:42:11 +0100

    feat: User authentication -add username and password to user model + schema

--- models/user.py
+++ models/user.py
@@ -4,9 +4,12 @@ class UserModel(db.Model):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+
     first_name = db.Column(db.String(80), nullable=False)
     last_name = db.Column(db.String(80), nullable=False)
-    birthdate = db.Column(db.DateTime, nullable=False)
+    birthdate = db.Column(db.Date, nullable=False)
 
     properties = db.relationship("PropertyModel", back_populates="user", lazy="dynamic")
 
--- schemas.py
+++ schemas.py
@@ -5,6 +5,8 @@ from marshmallow import Schema, fields
 # Plain schemas 
 class PlainUserSchema(Schema):
     id = fields.Int(dump_only=True)
+    username = fields.Str(required=True)
+    password = fields.Str(required=True, load_only=True)
     first_name = fields.Str(required=True)
     last_name = fields.Str(required=True)
     birthdate = fields.Date(required=True)
